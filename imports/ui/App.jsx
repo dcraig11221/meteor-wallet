b7ded0bc31ae2ac8064c530ef0f21a7e10236928
dcraig11221 <dcraig112@gmail.com>
2022-04-04 17:24:03 -0400

    changed name

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,7 +5,7 @@ import { ContactForm } from './ContactForm';
 
 export const App = () => (
   <div>
-    <h1>Contacts Manager!</h1>
+    <h1>Contacts Manager Plus!</h1>
     < ContactForm />
     <h4>Changes committed at 1:45pm</h4>
     < ContactList />
